068c6dd7fd81d66475ff94453a81b53e5439644b
qingqing <1429080837@qq.com>
2021-07-31 19:54:03 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+ <h2>我是弟弟的</h2>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+}
+</script>
+
+<style>
+
+
+</style>
+
+
+
